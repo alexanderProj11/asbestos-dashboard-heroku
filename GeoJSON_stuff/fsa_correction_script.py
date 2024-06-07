59336fa56f747630848767d7c497a295c7e86760
alexanderProj11 <salaa@myumanitoba.ca>
2024-06-07 14:25:15 +0000

    Changed

--- GeoJSON_stuff/fsa_correction_script.py
+++ GeoJSON_stuff/fsa_correction_script.py
@@ -3,7 +3,7 @@ import geopandas as gpd
 from shapely.geometry import Point
 
 # Load the CSV file
-csv_file = 'all_Valid_Addresses_2.csv'
+csv_file = 'all_Valid_Addresses_3.csv'
 df = pd.read_csv(csv_file)
 
 # Load the GeoJSON file
